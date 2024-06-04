58cb6401350d3ea79785b52bdfe4d793edbccc70
Cléber <clebersevero1998@gmail.com>
2024-06-04 18:07:45 -0300

    feat: get categories names

--- src/paginas/Categoria/index.tsx
+++ src/paginas/Categoria/index.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react"
 import TituloPrincipal from "../../componentes/TituloPrincipal"
+import { ICategoria } from "../../interfaces/ICategoria"
+import { http } from "../../http"
+import { useParams } from "react-router-dom"
 
 const Categoria = () => {
+  const [categoria, setCategoria] = useState<ICategoria>()
+  
+  const params = useParams()
+
+  useEffect(() => {
+    http.get('categorias', { params: { slug: params.slug } })
+      .then(res => { setCategoria(res.data[0]) })
+  }, [params.slug])
+
   return(
     <section>
-      <TituloPrincipal titulo="Categoria" />
+      <TituloPrincipal titulo={categoria?.nome ?? ''} />
     </section>
   )
 }
