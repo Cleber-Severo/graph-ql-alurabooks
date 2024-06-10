1f9c0e67e26a926cc5738919de85fa5e00f9261f
Cléber <clebersevero1998@gmail.com>
2024-06-10 13:44:26 -0300

    feat: adding react query to fetch categories

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,39 +1,46 @@
 import axios, { AxiosError } from "axios";
 import { useObterToken } from "../hooks/useToken";
+import { ICategoria } from "../interfaces/ICategoria";
 
 
 export const http = axios.create({
-    baseURL: 'http://localhost:8000',
-    headers: {
-        Accept: 'aplication/json',
-        Content: 'application/json'
-    }
+  baseURL: 'http://localhost:8000',
+  headers: {
+    Accept: 'aplication/json',
+    Content: 'application/json'
+  }
 })
 
 http.interceptors.request.use(function (config) {
-    // Do something before request is sent
-    const token = useObterToken()
+  // Do something before request is sent
+  const token = useObterToken()
 
-    if(token && config.headers) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
 
-    return config;
-  }, function (error) {
-    // Do something with request error
-    console.log('Erro na instancia axios - Interceptor');
-    
+  return config;
+}, function (error) {
+  // Do something with request error
+  console.log('Erro na instancia axios - Interceptor');
 
-    return Promise.reject(error);
+
+  return Promise.reject(error);
 });
 
 http.interceptors.response.use(function (response) {
   // Qualquer código de status que esteja dentro do intervalo de 2xx faz com que esta função seja acionada
-   // Faça algo com os dados de resposta
+  // Faça algo com os dados de resposta
   return response;
 }, function (error: AxiosError) {
   // Qualquer código de status que esteja fora do intervalo de 2xx faz com que esta função seja acionada
-   //Faça algo com erro de resposta
+  //Faça algo com erro de resposta
 
   return Promise.reject(error);
 });
+
+export const obterCategoriaPorSlug = async (slug: string) => {
+  const resposta = await http.get<ICategoria[]>('categorias', { params: { slug: slug } })
+  
+  return resposta.data[0]
+}
--- src/paginas/Categoria/index.tsx
+++ src/paginas/Categoria/index.tsx
@@ -1,27 +1,17 @@
-import { useEffect, useState } from "react"
 import TituloPrincipal from "../../componentes/TituloPrincipal"
-import { ICategoria } from "../../interfaces/ICategoria"
-import { http } from "../../http"
+import { obterCategoriaPorSlug } from "../../http"
 import { useParams } from "react-router-dom"
 import Loader from "../../componentes/Loader"
+import { useQuery } from "@tanstack/react-query"
 
-const Categoria = () => {
-  const [categoria, setCategoria] = useState<ICategoria>()
-  const [ estaCarregando, setEstaCarregando  ] = useState(false)
-  
+const Categoria = () => {  
   const params = useParams()
 
-  useEffect(() => {
-    setEstaCarregando(true)
-    http.get('categorias', { params: { slug: params.slug } })
-      .then(res => { 
-        setCategoria(res.data[0]) 
-        setEstaCarregando(false)
-      })
-  }, [params.slug])
+  const { data: categoria, isLoading } = useQuery(['categoria'], () => obterCategoriaPorSlug(params.slug || ''))
 
-  if(estaCarregando) {
-    return (<Loader />)
+
+  if(isLoading) {
+    return <Loader />
   }
 
   return(
